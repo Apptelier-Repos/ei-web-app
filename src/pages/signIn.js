6f881ce3f4630c63275243ebcfa30f5094752e7a
Lesair Valmont <ixra.net@gmail.com>
2019-09-08 15:08:21 -0500

    [AB#152] Testing Material-UI.
    Making sure Material-UI and theme are working correctly.

--- src/pages/signIn.js
+++ src/pages/signIn.js
@@ -1,9 +1,27 @@
 import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import withRoot from "../withRoot";
+import Typography from "@material-ui/core/Typography";
+
+const styles = theme => ({
+  root: {
+    height: "100vh"
+  }
+});
 
 class SignIn extends React.Component {
   render() {
-    return <p>Sign in page.</p>;
+    return (
+      <React.Fragment>
+        <Typography>Sign in page.</Typography>
+      </React.Fragment>
+    );
   }
 }
 
-export default SignIn;
+SignIn.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withRoot(withStyles(styles)(SignIn));
